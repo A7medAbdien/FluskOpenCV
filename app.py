0490528ce7725b9f5b54c42aad27e28866d52da2
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-11-09 14:50:04 +0300

    redirect and url_for

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 """
 this is the WSGL applicaiton,
@@ -18,6 +18,27 @@ it takes us to this url
 @app.route('/')
 def welcome():
     return "Hi there! WOW"
+
+
+@app.route('/<int:mark>')
+def mark(mark):
+    if mark > 50:
+        result = "gj"
+    else:
+        result = "bj"
+    # return result + ": " + str(mark)
+    # url_for(name_of_def,its_prams)
+    return redirect(url_for(result, score=mark))
+
+
+@ app.route('/gj/<int:score>')
+def gj(score):
+    return str(score)
+
+
+@ app.route('/bj/<int:score>')
+def bj(score):
+    return (score)
 
 
 if __name__ == '__main__':
