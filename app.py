8d7a6127911d7ed7aacccc57aa5003f5f2043ffe
Ahmed Abdin <ahmed.g.abdien@gmail.com>
2022-11-11 23:26:23 +0300

    yiled get_distance

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def extract_landmarks(landmarks):
 
 
 def get_distance(a, b):
-    return math.sqrt(
+    yield math.sqrt(
         (a[0]-b[0])**2 +
         (a[1]-b[1])**2 +
         (a[2]-b[2])**2)
